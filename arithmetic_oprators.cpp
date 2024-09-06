35c631d343d2fdef466e27089361614c27da3ea3
Parth Padmawar <parthpadmawar4@gmail.com>
2024-09-06 21:14:16 +0530

    About Arithmetic Opretors

---
+++ arithmetic_oprators.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int main(){
+
+    /* arithmetic_operators = return the result of a specific
+                              arithmetic opration (+ - * /)
+    */ 
+
+   
+
+    //int students = 20;
+
+    //students = students + 1;   //addition
+    //storter ways to write it.
+    //students+=1;
+    //students++;
+
+    //students = students - 1; //substraction
+    //students-=1;
+    //students--;
+
+    //students = students * 2; //multiplication
+    //students*=2;
+
+    //students = students / 3;  //divide
+    //students/=2;
+    
+    //int remainder = students % 2; //mojulus oprator
+
+
+    //std::cout << students;
+
+    //std::cout << remainder;
+
+    //parenthetis
+    //multiplication & division
+    //addition & subtraction
+
+    int students = 6 - 5 + 4 * 3 / 2;
+
+    std::cout << students;
+
+    
+
+    return 0;
+}
