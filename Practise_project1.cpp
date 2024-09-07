2c39c3bdf81282027cb3588caea79a9c60243b0e
Parth Padmawar <parthpadmawar4@gmail.com>
2024-09-07 18:52:13 +0530

    Calculate hypotenuse of triangle

---
+++ Practise_project1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    double a;
+    double b;
+    double c;
+
+    std::cout << "Enter side A :  " ;
+    std::cin >> a;
+
+    std::cout << "Enter side B :  ";
+    std::cin >> b;
+
+    //a = pow(a, 2);
+    //b = pow(b, 2);
+    //c = sqrt(a+b);
+
+    c = sqrt(pow(a, 2) + pow(b, 2));    //another way of writing it 
+
+    std::cout << "Length of side C : " << c ;
+    return 0;
+
+}
