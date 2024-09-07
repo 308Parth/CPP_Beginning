528bea86f36a93a42ca5f2bf32fba841bde2a300
Parth Padmawar <parthpadmawar4@gmail.com>
2024-09-07 16:10:46 +0530

    Maths functions

---
+++ Maths_related_functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double x = 3.98;
+    double y = 4;
+    double z;
+
+    //z = std::max(x, y);
+    //z = std::min(x,y);
+    //z = pow(2, 3);   //This is how to add power. Here it means 2 to the power 3.
+    //z = sqrt(9);     //For square root
+    //z = abs(-3);     //Mod
+    //z = round(x);    //Approx
+    //z = ceil(x);     //Greatest integer*
+    //z = floor(x);    //will always return to the number which is infrount of .
+    
+    std::cout << z;
+
+    return 0;
+}
