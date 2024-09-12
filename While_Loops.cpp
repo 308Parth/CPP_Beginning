1a08dcb04245b0f9f5c197c30a4fb00bc3e60b3a
Parth Padmawar <parthpadmawar4@gmail.com>
2024-09-12 10:31:09 +0530

    While Loops

---
+++ While_Loops.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    std::string name;
+
+    /*while//This is used on the place of if statement in this way we can like creat a compulsion for user to enter someting by creating a loop
+    (name.empty()){
+        std::cout << "Enter your name: ";
+        std::getline(std::cin,name);
+    }*/
+
+   
+    std::cout << "Hello" << name;
+
+
+    /*while(1==1) //(Infinite loop)
+    {std::cout << "Help! I AM STUCK IN AN INFINITW LOOP";}*/
+    
+
+    return 0;
+}
