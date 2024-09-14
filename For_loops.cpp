22ebf62a2a1a0a203fe89ed000991ad5f0ce8404
Parth Padmawar <parthpadmawar4@gmail.com>
2024-09-14 22:30:50 +0530

    For loop (counting , sentance repeat

---
+++ For_loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    /*for(int i = 1; i <= 5; i++){   //Repeat the sentace i number of times
+        std::cout << "HAPPY NEW YEAR!\n";
+    }*/
+   /*for(int i = 0; i<= 10; i+=2){  //Counting Accending 'i+=x' for multiples
+    std::cout << i << '\n';
+   }
+   std::cout << "HAPPY NEW YEAR!\n";*/
+
+   for(int i = 10; i >= 0; i-=2){  //Counting Decending 
+    std::cout << i << '\n';
+   }
+
+    return 0;
+}
