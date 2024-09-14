e3a7bfa254d0eb46dd894e51d744586f1d29d6d5
Parth Padmawar <parthpadmawar4@gmail.com>
2024-09-14 22:44:10 +0530

    Break & Continue

---
+++ Break_Continue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+   // break = break out of a loop 
+   // continue = skip current iteration
+   
+   for(int i = 1; i <= 20; i++){
+        if(i == 13){
+            continue;
+        }
+        std::cout << i << '\n';
+   }
+
+    return 0;
+}
